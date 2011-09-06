e0ec66613ef27b7074ce3ddcdf80ff53de743cec
muatik <muatik@gmail.com>
2011-09-06 04:34:42 +0300

    konfigürasyon dosyası oluşturuldu

---
+++ _config.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','on');
+ini_set('time_zone','on');
+ini_set('mbstring.internal_encoding','UTF-8');
+date_default_timezone_set('Europe/Istanbul');
+
+/*
+ * veritabanı sabit değişkenleri tanımlanıyor
+ * */
+define('_dbHost','localhost');
+define('_dbUser','root');
+define('_dbPassword','root');
+define('_dbDatabase','cuzdan');
+
+?>
