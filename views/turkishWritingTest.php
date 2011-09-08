e202fc835d1fe92ba2c4666589929fca95558a9f
ErmanGulhan <mr.ermangulhan@gmail.com>
2011-09-09 00:40:54 +0300

    Görüntü sayfaları stilleri ile birlikte tamamen geliştirildi.

---
+++ views/turkishWritingTest.php
@@ -0,0 +1,62 @@
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="../css/reset.css" />
+<link rel="stylesheet" type="text/css" href="../css/common.css" />
+<link rel="stylesheet" type="text/css" href="../css/testPage.css" />
+<style type="text/css">
+.turkishWritingTest{
+	padding:10px;
+	width:600px;
+}
+.turkishWritingTest input.correct{
+	color:green;
+}
+.turkishWritingTest input.incorrect{
+	color:red;
+	text-decoration:line-through;
+}
+.turkishWritingTest a{
+	text-decoration:underline;
+}
+</style>
+
+<div class="turkishWritingTest">
+	<div class="testPageHeader">
+		<h1>Türkçesini Yazma Testi</h1>
+		<p>
+			Toplam soru:<span class="totalQuestions">21</span>,
+			Tahmini süre:<span class="estimatedTime">10 dakika</span>,
+		</p>
+		<p>
+			Geçen süre:<span class="spentTime">00:00:05</span>,
+			Doğru sayısı:<span class="correctAnswers">0</span>,
+			Yanlış sayısı:<span class="incorrectAnswers">1</span>,
+			Boş:<span class="emptyQuestions">11</span>
+		</p>
+	</div>
+	<ol class="testPageOl">
+		<li>
+			<p>
+				<input type="text" class="incorrect" value="schedule" />
+				<span class="categories">[v, n]</span>
+				<span class="meanings">
+					perfect, elegant, outstanding
+				</span>
+			</p>
+			<p class="incorrect">
+				<strong>Doğrusu:</strong><a href="#">doğru kelime</a>
+				<span class="seperator">|</span>
+				<strong>Yanlış:</strong>
+				<span class="meaningOfIncorrect">program</span>
+			</p>
+		</li>
+		<li>
+			<p>
+				<input type="text" class="correct" value="excellent" />
+				<span class="categories">[v, n]</span>
+				<span class="meanings">
+					perfect, elegant, outstanding
+				</span>
+			</p>
+		</li>
+	</ol>
+</div>
---
+++ views/variationWritingTest.php
@@ -0,0 +1,69 @@
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="../css/reset.css" />
+<link rel="stylesheet" type="text/css" href="../css/common.css" />
+<link rel="stylesheet" type="text/css" href="../css/testPage.css" />
+<style type="text/css">
+.variationWritingTest{
+	padding:10px;
+	width:600px;
+}
+.variationWritingTest a{
+	text-decoration:underline;
+}
+.variationWritingTest label{
+	font-weight:normal;
+	text-align:right;
+	display:inline-block;
+	width:215px;
+}
+.variationWritingTest input[type=submit]{
+	margin-top:-5px;
+}
+</style>
+
+<div class="variationWritingTest">
+	<div class="testPageHeader">
+		<h1>Kelimenin Varyasyonlarını Yazma</h1>
+		<p>
+			Toplam soru:<span class="totalQuestions">21</span>,
+			Tahmini süre:<span class="estimatedTime">10 dakika</span>,
+		</p>
+		<p>
+			Geçen süre:<span class="spentTime">00:00:05</span>,
+			Doğru sayısı:<span class="correctAnswers">0</span>,
+			Yanlış sayısı:<span class="incorrectAnswers">1</span>,
+			Boş:<span class="emptyQuestions">11</span>
+		</p>
+	</div>
+	<ol class="testPageOl">
+		<li>
+			<strong>catastrophe</strong>
+			<ul>
+				<li>
+					<label>Noun:<input type="text" /></label>
+					<img src="../images/correct.png" alt="" />
+				</li>
+				<li>
+					<label>Adjective:<input type="text" /></label>
+					<img src="../images/correct.png" alt="" />
+				</li>
+				<li>
+					<label>Adverb:<input type="text" /></label>
+					<img src="../images/incorrect.png" alt="" />
+					<span>catastropically</span>
+				</li>
+			</ul>
+			<input type="submit" value="Tamam" />
+		</li>
+		<li>
+			<strong>initial</strong>
+			<ul>
+				<li>
+					<label>Adverb:<input type="text" /></label>
+				</li>
+			</ul>
+			<input type="submit" value="Tamam" />
+		</li>
+	</ol>
+</div>
+
