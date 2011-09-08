26a627b2f5a8389be2f3eadb2531c74940c70a61
ErmanGulhan <mr.ermangulhan@gmail.com>
2011-09-09 00:38:32 +0300

    Bazı stil dosyaları değiştirildi ve yenileri eklendi.

--- views/sentenceCompletionTest.php
+++ views/sentenceCompletionTest.php
@@ -1,9 +1,32 @@
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="../css/reset.css" />
+<link rel="stylesheet" type="text/css" href="../css/common.css" />
+<link rel="stylesheet" type="text/css" href="../css/testPage.css" />
 <style type="text/css">
-
+.sentenceCompletionTest{
+	padding:10px;
+	width:600px;
+}
+.sentenceCompletionTest input{
+	margin:0 8px 0 8px;
+}
+.sentenceCompletionTest input.correct{
+	color:green;
+}
+.sentenceCompletionTest input.incorrect{
+	color:red;
+	text-decoration:line-through;
+}
+.sentenceCompletionTest a{
+	text-decoration:underline;
+}
+.sentenceCompletionTest .testPageOl p span:first-child{
+	margin-left:0px;
+}
 </style>
 
 <div class="sentenceCompletionTest">
-	<div><!-- border-bottom:1 px solid gray; -->
+	<div class="testPageHeader">
 		<h1>Cümle Tamamlama Testi</h1>
 		<p>
 			Toplam soru:<span class="totalQuestions">21</span>,
@@ -16,14 +39,15 @@
 			Boş:<span class="emptyQuestions">11</span>
 		</p>
 	</div>
-	<ol>
+	<ol class="testPageOl">
 		<li>
-			<p>This is not<input type="text" />of us.</p>
-			<p><span>ip ucu:</span><span>car, bike, go</span></p>
+			<p>This is not<input type="text" class="correct" value="go" />of us.</p>
+			<p class="hint"><span>ip ucu:</span><em>car, bike, go</em></p>
 		</li>
 		<li>
-			<p>Where are<input type="text" />going?</p>
-			<p><span>ip ucu:</span><span>I, you, he</span></p>
+			<p>Where are<input type="text" class="incorrect" value="I" />going?</p>
+			<p class="hint"><span>ip ucu:</span><em>I, you, he</em></p>
+			<p>Doğrusu:<a href="#">you</a></p>
 		</li>
 	</ol>
 </div>
