13f625db99ce063b5150828256cb859bde66315c
alpayOnal <alpaycom@gmail.com>
2011-09-14 16:10:06 +0300

    dictionary parser geliştiriliyor.

--- crawlers/dictionary.php
+++ crawlers/dictionary.php
@@ -37,23 +37,21 @@ class crawler
 	
 	public function parseContent(){
 
-		print_r($this->getMeans());
+		
+		//print_r($this->getMeans());
+		
+		//$this->getEtymology();
 
-		// $this->getEtymology();
-
-		//$this->getSynonyms();
+		echo $this->getSynonyms();
 
 		//$this->getStructure();
 
 	}
 	
 	public function getEtymology(){
-
-		$elements=$this->domXPath->query("//*[@class='ety']");
-
-		foreach($elements as $nodes)		
-			$etymology=$nodes->nodeValue;
-		return $etymology;
+		preg_match('/<div class="dicTl">Word Origin & History<\/div[\w\s\S]*?([\s\S]*?<\/div>[\s\S]*?){4}/im',$this->content,$m);
+		preg_match_all('/(div class="body")(.*)(<\/div)/im',$m[0],$k);
+		return str_replace('div class="body">','',strip_tags($k[0][0]));
 	}
 	
 	public function getSynonyms(){
@@ -94,18 +92,32 @@ class crawler
 	}
 	
 	/**
-	 * ilgili kelimeyle ilgili sayfadaki türlere göre tüm anlamları çeker
+	 * ilgili kelimeyle ilgili anlamları ve türlerini verir.
 	 * 
-	 * @param elements	Görünür olan anlam elementlerinde null'dur.
-	 * 					Görünür olmayanlarda ise "moredef" gönderiliyor.
+	 * @return array
+	 * */
+	public function getMeans(){
+		return	array_merge($this->getMeansList(),$this->getMeansBody());
+	}
+	
+	/**
+	 * ilgili kelimeyle ilgili "pbk" classı içindeki  türlere göre tüm anlamları çeker
+	 * 
+	 * @param string 	$kind	Görünür olan anlam elementlerinde null'dur.
+	 * 							Görünür olmayanlarda ise "moredef" gönderiliyor.
+	 * @param domobject $node	"more" linkine tıklandığında açılan, 
+	 * 							gizli anlam listesini alır.
 	 * 				
 	 * @return 	array
 	 * */
-	public function getMeans($class="pbk",$kind=null){
+	public function getMeansList($kind=null,$node=null){
 		
+		// anlam dizisi
 		$means=array();
-		
-		$elements=$this->domXPath->query("//*[@class='".$class."']");
+		// node 
+		if ($node==null)
+			$elements=$this->domXPath->query("//*[@class='pbk']");
+		else $elements=$node->childNodes;
 		
 		// <tür ve anlamları kaydet>	
 		foreach($elements as $nodes){
@@ -116,16 +128,73 @@ class crawler
 				 * eğer daha fazla anlam linki varsa o linkin içeresindeki 
 				 * kayıtları yakalıyor ve diziye ekliyor
 				 * */
-				if ($node->getAttribute('class')=='moredef'){
-					$moredef=$this->getMeans("moredef",$kind);
-					$means[$kind]=array_merge($means[$kind],$moredef[$kind]);
-				}
+				if ($node->nodeName!='#text')
+					if ($node->getAttribute('class')=='moredef'){
+						$moredef=$this->getMeansList($kind,$nodes);
+						$means[$kind]=array_merge($means[$kind],$moredef[$kind]);
+					}
 				
 				if (!empty($node->nodeValue)){
 					
 					// tür değeri kontrol ediliyor ve alınıyor
 					if ($node->nodeName=='span' && $node->getAttribute('class')=='pg') 
 						$kind=str_replace('â','',$node->nodeValue);
+					else if ($node->nodeName=='div') {
+						
+						$dataX=$node->childNodes;
+						foreach($dataX as $dataC){
+							
+							if ($dataC->nodeName!='#text'){
+								
+								if ($dataC->getAttribute('class')=='dndata'){
+									if (empty($means[$kind])) $means[$kind]=array();
+									$means[$kind]=array_merge(
+											$means[$kind],
+											array($dataC->nodeValue)
+									);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return $means;
+	}
+	
+	/**
+	 * getmeans methodu gibi çalışır.sadece body class'ının bir altındaki
+	 * elementleri yaklar oradaki meanleri ve türlerini gönderir.
+	 * 			
+	 * @return 	array
+	 * */
+	public function getMeansBody(){
+		
+		$means=array();	
+		
+		$elements=$this->domXPath->query("//*[@class='body']");
+				
+		// <tür ve anlamları kaydet>
+		foreach($elements as $nodes){
+			
+			$childNodes = $nodes->childNodes;
+			foreach ($childNodes as $node) {
+				
+				/**
+				 * eğer daha fazla anlam linki varsa o linkin içeresindeki 
+				 * kayıtları yakalıyor ve diziye ekliyor
+				 * */
+				if ($node->nodeName!='#text'){
+				if ($node->getAttribute('class')=='moredef'){
+							$means=$this->getBodyMeanMore($node,$kind,$means);
+						}
+				}				
+				
+				if (!empty($node->nodeValue)){
+					
+					// tür değeri kontrol ediliyor ve alınıyor
+					if ($node->nodeName=='span' && $node->getAttribute('class')=='sectionLabel') 
+						$kind=$node->nodeValue;
 					else if ($node->nodeName=='div') {
 						
 						$dataX=$node->childNodes;
@@ -150,9 +219,51 @@ class crawler
 		return $means;
 	}
 	
+	/**
+	 * body class'ının bir altındaki "more" linkine tıklandığında açılan, 
+	 * sayfaki gizli anlamları listesini alır.
+	 * 
+	 * @param domobject $nodes 	gizli anlamları tutan dom objesi	 
+	 * @param string	$kind	gizli anlamların türü
+	 * @param array 	$means	görünen anlamları tutan dizi
+	 * 
+	 * @return array
+	 * */
+	public function getBodyMeanMore($nodes,$kind,$means){
+		
+		$childNodes = $nodes->childNodes;
+			foreach ($childNodes as $node) {						
+				
+				if (!empty($node->nodeValue)){
+					
+					// tür değeri kontrol ediliyor ve alınıyor
+					if ($node->nodeName=='span' && $node->getAttribute('class')=='sectionLabel') 
+						$kind=$node->nodeValue;
+					else if ($node->nodeName=='div') {
+						
+						$dataX=$node->childNodes;
+						foreach($dataX as $dataC){
+							
+							if ($dataC->nodeName!='#text'){
+								
+								if ($dataC->getAttribute('class')=='dndata'){
+									if (empty($means[$kind])) $means[$kind]=array();
+									$means[$kind]=array_merge(
+											$means[$kind],
+											array($dataC->nodeValue)
+									);
+									
+								}
+							}	
+						}
+					}
+				}	
+			}
+		return $means;
+	}
 }
 $c= new crawler();
-$c->pageRead('have');
+$c->pageRead('fast');
 
 ?>
 
