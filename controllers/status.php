87bb6abe17edcdd76103a657c17c4a0c0abc4b1e
ErmanGulhan <mr.ermangulhan@gmail.com>
2011-12-06 22:52:06 +0200

    Durum sayfası için gerekli sayfalar düzenlendi.

---
+++ controllers/status.php
@@ -0,0 +1,24 @@
+<?php
+require_once('ipage.php');
+class statusController extends ipage {
+
+	public function initialize(){
+		$this->title='Durum';
+		$this->autRequired=true;
+		parent::initialize();
+
+	}
+
+	public function run(){
+
+		if(!$this->isLogined){
+
+			header('location:/');
+
+		}
+
+		parent::run();
+	}
+}
+?>
+
