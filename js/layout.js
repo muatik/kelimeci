a5d083ec23b37d80d78348c39e5c39eb67d3a1b7
ErmanGulhan <mr.ermangulhan@gmail.com>
2011-12-17 03:11:53 +0200

    Üst kullanıcı menüsünde ve alt kullanıcı menüsündeki... bağlantılar ve formlarda değişiklikler yapıldı.

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,11 @@
 $(function(){
-
 	var
 		// User top menu
 		$usrMenu=$('#userTopMenu'),
 		// User sub top menu
-		$usrSubMenu=$('#userSubTopMenu');
+		$usrSubMenu=$('#userSubTopMenu'),
+		// Object to cancel time out 
+		timeOutObjForSubMenu=null;
 
 	// If the user menu clicked, show the sub user menu
 	$usrMenu.click(function(){
@@ -15,23 +16,42 @@ $(function(){
 		if($usrSubMenu.is(':hidden')){
 			// If it has any forms - user not logined/registered,
 			// 	toggle forms properly
-			if($usrSubMenu.find('form'))
+			if($usrSubMenu.find('form').length>0)
 				toggleFormsInUserMenu();
 
 			$usrSubMenu.show();
 		}
-		else
+		else{
 			$usrSubMenu.hide();
+			clearTimeout(timeOutObjForSubMenu);
+		}
 
 		return false;
 	});
+
+	// Hide the user sub menu within 1,25 second when it is leaved
+	$usrSubMenu.mouseleave(function(){
+		timeOutObjForSubMenu=setTimeout(
+			function(){$usrSubMenu.hide();},1250
+		);
+		return false;
+	});
+
+	// Cancel to hide the sub menu when it is entered
+	$usrSubMenu.mouseenter(function(){
+		clearTimeout(timeOutObjForSubMenu);
+	});
+
+	// If no any forms - user logined, don't do anthing for the forms
+	if($usrSubMenu.find('form').length<1)
+		return;
 
 	// Hide the register form on load
 	toggleFormsInUserMenu();
 
 	// Insert a link into the login form to show register form
 	$usrSubMenu.find('.loginForm .fInput:last')
-		.append('<a href="#" class="showRegisterForm">Hesap oluştur!</a>')
+		.append('<a href="#" class="showRegisterForm">&#187; Hesap oluştur</a>')
 		.find('a.showRegisterForm').click(function(){
 			toggleFormsInUserMenu('registerLink');
 			return false;
@@ -39,7 +59,7 @@ $(function(){
 
 	// Insert a link into the register form to show login form
 	$usrSubMenu.find('.registerForm .fInput:last')
-		.append('<a href="#" class="showLoginForm">Giriş formu!</a>')
+		.append('<a href="#" class="showLoginForm">&#187; Giriş formu</a>')
 		.find('a.showLoginForm').click(function(){
 			toggleFormsInUserMenu();
 			return false;
@@ -58,6 +78,14 @@ function toggleFormsInUserMenu(where){
 		$lFrm=$('#userSubTopMenu .loginForm');
 		// Register form
 		$rFrm=$('#userSubTopMenu .registerForm');
+
+	// Reset the forms
+	$lFrm.get(0).reset();
+	$rFrm.get(0).reset();
+
+	// Hide the form alerts
+	$lFrm.find('.frmAlert').hide();
+	$rFrm.find('.frmAlert').hide();
 	
 	if(where=='registerLink'){
 		$lFrm.hide();
--- views/userTopMenu.php
+++ views/userTopMenu.php
@@ -3,7 +3,7 @@
 <?php
 // Set the user menu with its sub menu
 if(!$o->isLogined){
-	$menu='Giriş yap';
+	$menu='Giriş yap / Hesap oluştur';
 
 	$o2=new stdClass();
 	$o2->noCss=1;
@@ -17,15 +17,28 @@ else{
 	$menu=$this->u->email;
 	$subMenu='
 	<ul id="userSubTopMenu">
-		<li><a href="#">Ayarlar</a></li>	
-		<li><a href="#">Çıkış yap</a></li>	
+		<li><a href="#"><img src="images/settings.png" alt="" />Ayarlar</a></li>	
+		<li><a href="profile"><img src="images/profile.png" alt="" />Profil</a></li>	
+		<li><a href="?_ajax=users/logout"><img src="images/logout.png" alt="" />Çıkış yap</a></li>	
 	</ul>';
 }
 // Print user menu and its sub menu
 echo '<a href="#" id="userTopMenu">'.$menu.'<img src="images/downArrow.png" alt="" /></a>';
 echo $subMenu;
+
+echo '<script type="text/javascript">';
+if(!$o->isLogined){
+	echo "
+		bindLoginForm($('#userSubTopMenu .loginForm'));
+		bindRegisterForm($('#userSubTopMenu .registerForm'));
+
+		// Set the font uppercase for the login and register form
+		$('#userSubTopMenu .loginForm .frmTitle').text('ÜYE GİRİŞİ');
+		$('#userSubTopMenu .registerForm .frmTitle').text('HESAP OLUŞTUR');
+	";
+}
+else{
+	echo "$('#userSubTopMenu').css('width',$('#userTopMenu').outerWidth()+'px')";
+}
+echo '</script>';
 ?>
-<script type="text/javascript">
-bindLoginForm($('#userSubTopMenu .loginForm'));
-bindRegisterForm($('#userSubTopMenu .registerForm'));
-</script>
