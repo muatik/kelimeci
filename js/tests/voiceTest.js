ecbb7382ae7cee54453754063ed377793b12bd55
ErmanGulhan <mr.ermangulhan@gmail.com>
2012-01-20 22:16:30 +0200

    Ses dosyalarının çalınması için modül eklendi. Voice Test'indeki ses dosyaları için bu modülle beraber geliştirme yapıldı.

--- js/tests/voiceTest.js
+++ js/tests/voiceTest.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
 
-	var test=new Test('voiceTest');
+	var 
+		test=new Test('voiceTest'),
+		// jPlayer used on the voice test page
+		$jplayer=$('.voiceTest voiceTest.jPlayer');
+
+	// Attach only one jPlayer to the page
+	$jplayer.jPlayer(voiceTestPage.jPlayer.ops);
 	
 	test.bindItems=function(){
 		
@@ -22,11 +28,13 @@ $(document).ready(function(){
 
 		});
 		
-		$('.testPageOl li img.voiceIcon').click(function(){
-			
-			// CODE FOR PLAYING THE VOICE FILE
-			$(this).addClass('playing');
-			
+		$('.testPageOl li img.voiceStatusImg').click(function(){
+			var
+				$item=$(this).parent(),
+				$voiceFile=$item.find('input.voiceFile');
+
+			// Set the voice file to the jplayer for the current question word
+			$jplayer.jPlayer('setMedia',{mp3:$voiceFile.val()});
 		});
 
 	}
@@ -72,3 +80,58 @@ $(document).ready(function(){
 	test.start();
 
 });
+
+// Voice test page options
+var voiceTestPage={};
+
+voiceTestPage.pageOps={
+	voiceFilesDir:'../audio/',
+	$items:$('.voiceTest .testPageOl li'),
+	$voiceStatusImgs:$('.voiceTest .testPageOl li img.voiceStatusImg'),
+	voiceStatusImg:{
+		dir:'../images/',
+		types:{
+			progress:dir+'voiceImgProgress.png',
+			play:dir+'voiceImgPlay.png',
+			stop:dir+'voiceImgStop.png'
+		}
+	}
+};
+
+// Options for jplayer that is used in the voice test page
+voiceTestPage.jPlayer={
+	// Init. options of jplayer
+	ops:{
+		swfPath:'jPlayer/jPlayer.swf',
+		supplied:'mp3',
+		solution:'flash,html',
+		progress:function(e){
+			voiceTestPage.jPlayer.methods.updateVoiceStatusImg(e,'progress');
+		},
+		play:function(e){
+			voiceTestPage.jPlayer.methods.updateVoiceStatusImg(e,'play');
+		},
+		ended:function(e){
+			voiceTestPage.jPlayer.methods.updateVoiceStatusImg(e,'ended');
+		}
+	},
+	// Custom methods for jplayer
+	methods:{
+		updateVoiceStatusImg:function(e,status){
+			var 
+				curVoiceFile=e.jPlayer.status.src,
+				$curVoiceImg=$voiceStatusImgs.filter('[src="'+curVoiceFile+'"]'),
+				newStatusImgTypeSrc=null;
+
+			if(status=='progress') 
+				newStatusImgTypeSrc=voiceTestPage.pageOps.voiceStatusImg.types.progress;
+			else if(status=='play') 
+				newStatusImgTypeSrc=voiceTestPage.pageOps.voiceStatusImg.types.stop;
+			else if(status=='ended') 
+				newStatusImgTypeSrc=voiceTestPage.pageOps.voiceStatusImg.types.play;
+
+			$curVoiceImg.attr('src',newStatusTypeSrc);
+		}
+	}
+};
+
